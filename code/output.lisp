6d615f39abeb1dc893b89b25b00ab795bc028492
Jan Moringen <jan.moringen@med.uni-greifswald.de>
2025-02-19 15:23:39 +0100

    Hack versioned output into general EMIT methods

--- code/output.lisp
+++ code/output.lisp
@@ -1,5 +1,14 @@
 (cl:in-package #:model-info-generator)
 
+;;; HACK(jmoringe): should not be an `:around' method
+(defmethod emit :around ((element data-model) (format t) (target pathname))
+  (assert (uiop:directory-pathname-p target))
+  (let* ((version          (remove #\. (version element)))
+         (versioned-target (merge-pathnames
+                            (make-pathname :directory `(:relative ,version))
+                            target)))
+    (call-next-method element format versioned-target)))
+
 (defmethod emit ((element data-model) (format t) (target pathname))
   (assert (uiop:directory-pathname-p target))
   (ensure-directories-exist target)
@@ -9,9 +18,10 @@
        (let* ((name       (name element))
               (class-name (translate-class-name name))
               (type       (string-downcase format))
-              (pathname   (make-pathname :name class-name
-                                         :type type)))
-         (emit element format (merge-pathnames pathname target)))))
+              (filename   (make-pathname :name class-name
+                                         :type type))
+              (pathname   (merge-pathnames filename target)))
+         (emit element format pathname))))
    (tables element)))
 
 (defmethod emit ((element table) (format t) (target pathname))
